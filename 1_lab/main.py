d91d08ecc5c7c161dfe85d0b4dce6383cd9841b9
UltraUberTurk <morpeh2005ka@gmail.com>
2025-09-13 15:07:04 +0300

    Небольшой фикс кода первой лабы

--- 1_lab/main.py
+++ 1_lab/main.py
@@ -1,6 +1,8 @@
 """Main file"""
 
-from utils import calculate_entropy, calculate_private_entropy, make_array
+from math import log2
+
+from utils import calculate_entropy, make_array
 
 
 def step() -> None:
@@ -12,15 +14,14 @@ def step() -> None:
     print("Массив вероятностей появления совокупности дискретных сообщений:")
     print(" ".join([str(i) for i in array]))
     print(f"Кол-во информации I(X) = {calculate_entropy(array)}")
-    print(
-        f"Максимальная частная энтропия {max([calculate_private_entropy(i) for i in array])}"
-    )
 
 
 def main() -> None:
     """
     Main function
     """
+    print(f"Максимальная энтропия {log2(8)}")
+
     for i in range(int(input("Сколько экспериментов нужно провести?"))):
         print()
         print(f"Эксперимент {i + 1}")
